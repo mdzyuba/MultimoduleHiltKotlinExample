df5e0b8c83667bae16e5cc283fe9bb543689fc9b
Mykola Dzyuba <7852483+mdzyuba@users.noreply.github.com>
2022-07-27 20:17:18 -0700

    Move moduleB to libraries.

--- app/sampleapp/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/sampleapp/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.sampleapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
--- libraries/moduleB/src/main/java/com/example/moduleb/ModuleBMainActivity.kt
+++ libraries/moduleB/src/main/java/com/example/moduleb/ModuleBMainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.moduleb
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 class ModuleBMainActivity : AppCompatActivity() {
